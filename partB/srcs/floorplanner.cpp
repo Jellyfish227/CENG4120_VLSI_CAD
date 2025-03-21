1c3c60c5077e143435c593c470d2b1ee49ba84f5
C. H. Yu <chyu2@cse.cuhk.edu.hk>
2025-03-21 22:06:53 +0800

    finish

--- partB/srcs/floorplanner.cpp
+++ partB/srcs/floorplanner.cpp
@@ -51,11 +51,11 @@ struct Block
 
 struct GraphVertex
 {
-    int x, y;
+    int w, h;
     int lShape, rShape;                                                                 // index of the left and right child
     Rotation rotation;                                                                  // original or rotated for leaf block graph
-    GraphVertex(int x, int y, int l, int r) : x(x), y(y), lShape(l), rShape(r) {}       // constructor for internal node graph
-    GraphVertex(int x, int y, Rotation r) : x(x), y(y), rotation(r) {}                  // constructor for leaf node graph
+    GraphVertex(int w, int h, int l, int r) : w(w), h(h), lShape(l), rShape(r) {}       // constructor for internal node graph
+    GraphVertex(int w, int h, Rotation r) : w(w), h(h), rotation(r) {}                  // constructor for leaf node graph
 };
 
 struct Node
@@ -65,8 +65,8 @@ struct Node
     Block *block;                       // pointer to the block/module (only for leaf nodes)
     Node *left;                         // pointer to the left child node
     Node *right;                        // pointer to the right child node
-    vector<GraphVertex> shape_curve;    // vector of GraphVertex objects representing the shape curve
-    int chosen_index;                   // index of chosen shape
+    vector<GraphVertex> shapeCurve;    // vector of GraphVertex objects representing the shape curve
+    int minGraphIndex;                   // index of chosen shape
 
     Node(char v, NodeType t) : type(t), value(v), left(nullptr), right(nullptr) {}
 };
@@ -74,14 +74,14 @@ struct Node
 /**
  * Recursively constructs a slicing tree from a stack of NPE tokens
  *
- * @param npe_stack Stack containing NPE tokens in postfix notation
+ * @param npeStack Stack containing NPE tokens in postfix notation
  * @param blocks Vector of blocks/modules to be placed in the floorplan
  * @return Root node of the constructed slicing tree
  */
-Node *buildTree(stack<Node *> &npe_stack, vector<Block> &blocks)
+Node *buildTree(stack<Node *> &npeStack, vector<Block> &blocks)
 {
-    Node *root = npe_stack.top();
-    npe_stack.pop();
+    Node *root = npeStack.top();
+    npeStack.pop();
     if (root->type == NodeType::LEAF)
     {
         root->block = &blocks.at(root->value);
@@ -90,9 +90,9 @@ Node *buildTree(stack<Node *> &npe_stack, vector<Block> &blocks)
     {
         // cout << "[debug: buildTree] operator " << root->value << endl;
         // cout << "[debug: buildTree] build right" << endl;
-        root->right = buildTree(npe_stack, blocks);
+        root->right = buildTree(npeStack, blocks);
         // cout << "[debug: buildTree] build left" << endl;
-        root->left = buildTree(npe_stack, blocks);
+        root->left = buildTree(npeStack, blocks);
     }
     return root;
 }
@@ -120,7 +120,7 @@ void compute_shape_curve(Node *root)
 
     if (root->type == LEAF)
     {
-        root->shape_curve = {
+        root->shapeCurve = {
             GraphVertex(root->block->shapes.at(Rotation::ORIGINAL).first, root->block->shapes.at(Rotation::ORIGINAL).second, Rotation::ORIGINAL),
             GraphVertex(root->block->shapes.at(Rotation::ROTATED).first, root->block->shapes.at(Rotation::ROTATED).second, Rotation::ROTATED)
         };
@@ -128,26 +128,26 @@ void compute_shape_curve(Node *root)
     else
     {
         vector<GraphVertex> dp;
-        vector<GraphVertex> left_curve = root->left->shape_curve;
-        vector<GraphVertex> right_curve = root->right->shape_curve;
+        vector<GraphVertex> leftCurve = root->left->shapeCurve;
+        vector<GraphVertex> rightCurve = root->right->shapeCurve;
 
         // DP: generate all combinations and store in each internal node
-        for (int i = 0; i < left_curve.size(); i++)
+        for (int i = 0; i < leftCurve.size(); i++)
         {
-            for (int j = 0; j < right_curve.size(); j++)
+            for (int j = 0; j < rightCurve.size(); j++)
             {
-                int x, y;
+                int w, h;
                 if (root->value == '*')
                 {
-                    x = left_curve.at(i).x + right_curve.at(j).x;
-                    y = max(left_curve.at(i).y, right_curve.at(j).y);
+                    w = leftCurve.at(i).w + rightCurve.at(j).w;
+                    h = max(leftCurve.at(i).h, rightCurve.at(j).h);
                 }
                 else if (root->value == '+')
                 { // '+'
-                    x = max(left_curve.at(i).x, right_curve.at(j).x);
-                    y = left_curve.at(i).y + right_curve.at(j).y;
+                    w = max(leftCurve.at(i).w, rightCurve.at(j).w);
+                    h = leftCurve.at(i).h + rightCurve.at(j).h;
                 }
-                dp.emplace_back(x, y, i, j);
+                dp.emplace_back(w, h, i, j);
             }
         }
 
@@ -155,7 +155,7 @@ void compute_shape_curve(Node *root)
         sort(dp.begin(), dp.end(),
              [](const GraphVertex &a, const GraphVertex &b)
              {
-                 return (a.x == b.x) ? (a.y < b.y) : (a.x < b.x);
+                 return (a.w == b.w) ? (a.h < b.h) : (a.w < b.w);
              });
 
         // Filter and keep non-increasing points
@@ -163,17 +163,17 @@ void compute_shape_curve(Node *root)
         if (!dp.empty())
         {
             result.push_back(dp.at(0));
-            int min_y = dp.at(0).y;
+            int minH = dp.at(0).h;
             for (size_t k = 1; k < dp.size(); ++k)
             {
-                if (dp.at(k).y < min_y)
+                if (dp.at(k).h < minH)
                 {
                     result.push_back(dp.at(k));
-                    min_y = dp.at(k).y;
+                    minH = dp.at(k).h;
                 }
             }
         }
-        root->shape_curve = result; // store finalized dp result
+        root->shapeCurve = result; // store finalized dp result
     }
 }
 
@@ -181,7 +181,7 @@ void compute_shape_curve(Node *root)
  * @brief Sets the chosen orientation for each block in the floorplan
  *
  * Recursively traverses the slicing tree and sets the orientation of each block
- * based on the chosen_index of each node. For leaf nodes, it directly sets the
+ * based on the minGraphIndex of each node. For leaf nodes, it directly sets the
  * block's orientation. For internal nodes, it recursively processes left and right
  * subtrees.
  *
@@ -191,14 +191,14 @@ void setOrientation(Node *root)
 {
     if (root->type == LEAF)
     {
-        root->block->orientation = root->shape_curve.at(root->chosen_index).rotation;
+        root->block->orientation = root->shapeCurve.at(root->minGraphIndex).rotation;
     }
     else
     {
-        int left_idx = root->shape_curve.at(root->chosen_index).lShape;
-        int right_idx = root->shape_curve.at(root->chosen_index).rShape;
-        root->left->chosen_index = left_idx;
-        root->right->chosen_index = right_idx;
+        int leftIdx = root->shapeCurve.at(root->minGraphIndex).lShape;
+        int rightIdx = root->shapeCurve.at(root->minGraphIndex).rShape;
+        root->left->minGraphIndex = leftIdx;
+        root->right->minGraphIndex = rightIdx;
         setOrientation(root->left);
         setOrientation(root->right);
     }
@@ -220,36 +220,36 @@ void setOrientation(Node *root)
  */
 void computeCood(Node *root, int x, int y)
 {
-    int idx = root->chosen_index;
+    int idx = root->minGraphIndex;
 
     if (root->type == LEAF)
     {
-        int w_m = root->block->shapes.at(root->block->orientation).first;
-        int h_m = root->block->shapes.at(root->block->orientation).second;
+        int wMod = root->block->shapes.at(root->block->orientation).first;
+        int hMod = root->block->shapes.at(root->block->orientation).second;
         // cout << "[debug: computeCood] node " << int(root->value) << " starting at (" << x << ", " << y << ")" << endl;
-        // cout << "[debug: computeCood] has width: " << w_m << " height: " << h_m << " orientation: " << root->block->orientation << endl;
+        // cout << "[debug: computeCood] has width: " << wMod << " height: " << hMod << " orientation: " << root->block->orientation << endl;
         root->block->coordinates = Coordinates(
             {x, y},            // Lower left
-            {x + w_m, y},      // Lower right
-            {x, y + h_m},      // Upper left
-            {x + w_m, y + h_m} // Upper right
+            {x + wMod, y},      // Lower right
+            {x, y + hMod},      // Upper left
+            {x + wMod, y + hMod} // Upper right
         );
     }
     else
     {
-        int left_idx = root->shape_curve.at(idx).lShape;
-        int w_left = root->left->shape_curve.at(left_idx).x;
-        int h_left = root->left->shape_curve.at(left_idx).y;
+        int leftIdx = root->shapeCurve.at(idx).lShape;
+        int wLeftMod = root->left->shapeCurve.at(leftIdx).w;
+        int hLeftMod = root->left->shapeCurve.at(leftIdx).h;
 
         if (root->value == '*')
         {
             computeCood(root->left, x, y);
-            computeCood(root->right, x + w_left, y);
+            computeCood(root->right, x + wLeftMod, y);
         }
         else
         { // '+'
             computeCood(root->left, x, y);
-            computeCood(root->right, x, y + h_left);
+            computeCood(root->right, x, y + hLeftMod);
         }
     }
 }
@@ -302,7 +302,7 @@ int main(int argc, char *argv[])
     }
 
     // Build the expression tree stack
-    stack<Node *> npe_stack;
+    stack<Node *> npeStack;
     int bCount = 0, oCount = 0;
     for (const auto &token : tokens)
     {
@@ -314,32 +314,32 @@ int main(int argc, char *argv[])
                 cerr << "Invalid NPE" << endl;
                 exit(EXIT_FAILURE);
             }
-            npe_stack.push(new Node(token[0], NodeType::INTERNAL));
+            npeStack.push(new Node(token[0], NodeType::INTERNAL));
         }
         else
         {
             bCount++;
-            npe_stack.push(new Node(stoi(token), NodeType::LEAF));
+            npeStack.push(new Node(stoi(token), NodeType::LEAF));
         }
     }
-    Node *root = buildTree(npe_stack, blocks);
+    Node *root = buildTree(npeStack, blocks);
 
     // Compute shape curves
     compute_shape_curve(root);
 
     // Find minimal area
-    int min_area = INT_MAX;
-    int best_idx = -1;
-    for (int i = 0; i < root->shape_curve.size(); ++i)
+    int minArea = INT_MAX;
+    int bestIdx = -1;
+    for (int i = 0; i < root->shapeCurve.size(); ++i)
     {
-        int area = root->shape_curve.at(i).x * root->shape_curve.at(i).y;
-        if (area < min_area)
+        int area = root->shapeCurve.at(i).w * root->shapeCurve.at(i).h;
+        if (area < minArea)
         {
-            min_area = area;
-            best_idx = i;
+            minArea = area;
+            bestIdx = i;
         }
     }
-    root->chosen_index = best_idx;
+    root->minGraphIndex = bestIdx;
 
     // Set module orientations
     setOrientation(root);
@@ -356,7 +356,7 @@ int main(int argc, char *argv[])
                << "(" << block.coordinates.upperLeft.first << " " << block.coordinates.upperLeft.second << ")" << " "
                << "(" << block.coordinates.upperRight.first << " " << block.coordinates.upperRight.second << ") " << endl;
     }
-    output << min_area << endl;
+    output << minArea << endl;
 
     input.close();
     output.close();
